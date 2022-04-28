d15fc19b7d0291b9e75c73d60d387067279990b8
ForestGump <eremeevt2004@gmail.com>
2022-04-28 19:47:26 +0400

    rename folder and add a new one

---
+++ JavaProjects/Sorts/Sorts.java
@@ -0,0 +1,18 @@
+public class Sorts
+{
+    public static void MergeSort()
+    {
+
+    }
+
+    public static void BubleSort()
+    {
+
+    }
+
+    public static void main()
+    {
+
+    }
+    
+}
