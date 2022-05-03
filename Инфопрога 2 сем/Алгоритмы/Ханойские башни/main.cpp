de11ac9d198bb7518e80902fc22dcf47824f4abf
ForestGump <eremeevt2004@gmail.com>
2022-05-03 23:39:59 +0400

    Update main.cpp

--- Инфопрога 2 сем/Алгоритмы/Ханойские башни/main.cpp
+++ Инфопрога 2 сем/Алгоритмы/Ханойские башни/main.cpp
@@ -2,23 +2,120 @@
 
 using namespace std;
 
-void move(char point1, char point2)
+void showBoard(int board[disk_kol][3],  int rows, int cols)
 {
-    std::cout << "Из стержня " << poin1 << " в стержнь " << point2 << std::endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (board[i][j] == 0)
+            {
+                std::cout << " \t";
+            }
+
+            else
+            {
+                std::cout << board[i][j] << "\t";
+            }
+        }
+        
+        std::cout << std::endl;
+    }
+
+    std::cout << "\n\n";
 }
 
-void moveTower(int amount, char point1, char, point 2, char temp)
+int getRow(int board[disk_kol][3], int rows, int nCol)
+{
+    int nRow = -1;
+
+    for (int i = 0; i < rows; i++)
+    {
+        if (board[i][nCol] != 0)
+        {
+            nRow = i;
+
+            break;
+        }
+    }
+
+    return nRow;
+}
+
+void move(char point1, char point2, int board[disk_kol][3], int rows, int cols)
+{
+    int col1 = (int)point1 - (int)'A';
+    int col2 = (int)point2 - (int)'A';
+
+    int row1 = getRow(board, rows, col1);
+    int row2 = getRow(board, rows, col2);
+
+    if (row2 == -1)
+    {
+        row2 = row - 1;
+    }
+
+    else
+    {
+        row2--;
+    }
+
+    int value = board[row1][col1];
+
+    board[row1][col1] = 0;
+    board[row2][col2] = value;
+
+    showBoard(board, rows, cols)
+}
+
+bool isEmpty(int board[]disk_kol[3], int rows, int cols)
+{
+    int lastRowIndex = row - 1;
+    
+    for (int j = 0l j < cols; j++)
+    {
+        if (board[lastRowIndex][j] != 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void initBoard(int diskCount, int boar[disk_kol][3], int rows, nCol)
+{
+    int lastRowIndex = rows - 1;
+
+    for (int i = lastRowIndex; diskCount > 0; i --)
+    {
+        board[i][nCol] = diskCount;
+
+        diskCount --;
+    }
+}
+
+void moveTower(int amount, char point1, char, point 2, char temp, int board[disk_kol][3], int rows, int cols)
 {
     if (amount == 0)
     {
         return;
     }
 
-    moveTower(amount - 1, temp, point2);
-    
-    move(point1, point2);
+    if (isEmpty(board, rows, cols))
+    {
+        initBoard(amount, board, rows, 0);
 
-    moveTower(amount - 1, temp, point2, point1);
+        std::cout << "Исходная позиция" << std::endl;
+
+        showBoard(board, rows, cols);
+    }
+
+    moveTower(amount - 1, temp, point1, point2, board, rows, cols);
+    
+    move(point1, point2, board, rows, cols);
+
+    moveTower(amount - 1, temp, point2, point1, board, rows, cols);
 }
 
 int main()
@@ -28,5 +125,10 @@ int main()
     std::cout << "Введите кол-во дисков" << std::endl << ">>";
     std::cin >> disk_kol;
 
-    moveTower(disk_kol, 'A', 'B', 'C');
+    const int rows = disk_kol;
+    const int cols = 3;
+
+    int board[rows][cols] = {0};
+
+    moveTower(disk_kol, 'A', 'B', 'C', board, rows, cols);
 }
