89ca3477cacf4b3e59f4d0050ac60c07b1ff0c32
EremeevTim2004 <eremeevt2004@gmail.com>
2022-05-07 14:38:16 +0400

    diferent algorithms of serching fibbonachchi's num

--- Инфопрога 2 сем/Алгоритмы/Числа Фиббоначчи/main.cpp
+++ Инфопрога 2 сем/Алгоритмы/Числа Фиббоначчи/main.cpp
@@ -2,7 +2,66 @@
 
 using namespace std;
 
+// Рекурентные соотношения
+/*void RecurentSerch(int nNum)
+{
+	//...
+}*/
+
+// Золотое сечение
+void GoldSerch(int nNum)
+{
+	int counter = 3;
+
+	int a = 1, b = 1, tmp;
+
+	while (counter < nNum)
+	{
+		tmp = a;
+		a = b + a;
+		b = tmp;
+
+		counter + = 1;
+	}
+
+	return a;
+}
+
+// Через матрицы
+/*void MatrixSerch(int nNum)
+{
+	//...
+}
+
+// 
+void Revizor(int res_1, int res_2, int res_3)
+{
+	if (res_1 < res_2 & res_1 < res_3)
+	{
+		std::cout << "RecurentSerch is won!" << std::endl;
+	}
+	else if (res_2 < res_1 & res_2 < res_3)
+	{
+		std::cout << "GoldSerch is won!" << std::endl;
+	}
+	else
+	{
+		std::cout << "MatrixSerch is won!" << std::endl;
+	}
+}*/
+
 int main()
 {
-    
+	int nNum;
+
+	std::cout << "" << std::endl << ">>";
+	std::cin >> nNum;
+
+	//RecurentSerch(nNum);
+	GoldSerch(nNum);
+	//MatrixSerch(nNum);
+
+	//Revizor();
+
+	return 0;
 }
