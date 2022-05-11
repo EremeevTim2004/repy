6f2ccac05a1f72c589c2485c08c6393a30aeea3e
ForestGump <eremeevt2004@gmail.com>
2022-05-11 17:41:16 +0400

    Update premake5.lua
    update for compiling new project

--- Creativ programming/cc-template/premake5.lua
+++ Creativ programming/cc-template/premake5.lua
@@ -88,3 +88,8 @@ project "ray-casting"
     location "src/%{prj.name}"
     files { "src/%{prj.name}/**.h", "src/%{prj.name}/**.hpp", "src/%{prj.name}/**.cpp" }
 
+project "ray-casting-editor"
+    language "C++"
+    cppdialect "C++17"
+    location "src/%{prj.name}"
+    files { "src/%{prj.name}/**.h", "src/%{prj.name}/**.hpp", "src/%{prj.name}/**.cpp" }
