9ee82e1ba6298b802e5fe17bee08a6e63db3f358
ForestGump <eremeevt2004@gmail.com>
2022-05-25 00:16:23 +0400

    Перемножение матриц через алгоритм Strassenа

--- Инфопрога 2 сем/Алгоритмы/Перемножение матриц/main.cpp
+++ Инфопрога 2 сем/Алгоритмы/Перемножение матриц/main.cpp
@@ -1,173 +1,60 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-
-#include "funcs.h"
+#include <vector>
 
 using namespace std;
 
-//
 int main()
 {
-	int voter_1; //
-
-	std::cout << "Whitch method do u prefer? (1 - Shtrassen; 2 - Vinigrad)" << std::endl << ">>";
-	std::cin >> voter_1;
-
-	//
-	switch(voter_1)
+	vector <vector <int>> A;
+	vector <vector <int>> B;
+	
+	// Заполнение матрицы А
+	for (int i = 0; i < 2; i++)
 	{
-		// Реализация через алгоритм Штрассена
-		case 1:
+		for (int j = 0; j < 2; j ++)
 		{
-			int voter_2;
-
-			std::cout << "" << std::endl << ">>";
-			std::cin >> voter_2;
-
-			switch(voter_2)
-			{
-				// by hand
-				case 1:
-				{
-					int row_A, col_A;
-
-					std::cout << "" << std::endl << ">>";
-					std::cin >> row_A >> col_A;
-
-					MatrixMakingHand(row_A, col_A);
-
-					int row_B, col_B;
-
-					std::cout << "" << std::endl << ">>";
-					std::cin >> row_B >> col_B;
-					
-					MatrixMakingHand(row_B, col_B);
-
-					ShtrassenAlgorithm(matrix_A, matirx_B, row_A, col_A, row_B, col_B);
-
-					break;
-				}
-
-				// from file
-				case 2:
-				{
-					std::string file_name;
-
-					std::cout << "" << std::endl << ">>";
-					std::cin >> file_name;
-
-					// тут будет работа с файлами
-
-					int row_A, col_A;
-
-					std::cout << "" << std::endl << ">>";
-					std::cin >> row_A >> col_A;
-
-					MatrixMakingFile(row_A, col_A);
-
-					int row_B, col_B;
-
-					std::cout << "" << std::endl << ">>";
-					std::cin >> row_B >> col_B;
-					
-					MatrixMakingFile(row_B, col_B);
-
-					ShtrassenAlgorithm(matrix_A, matirx_B, row_A, col_A, row_B, col_B);
-
-					break;
-				}
-
-				default:
-				{
-					std::cout << "" << std::endl;
-
-					break;
-				}
-			}
-
-			break;
-		}
-
-		// Реализация через алгоритм Винограда
-		case 2:
-		{
-			int voter_2;
-
-			std::cout << "" << std::endl << ">>";
-			std::cin >> voter_2;
-
-			switch(voter_2)
-			{
-				// by hand
-				case 1:
-				{
-					int row_A, col_A;
-
-					std::cout << "" << std::endl << ">>";
-					std::cin >> row_A >> col_A;
-
-					MatrixMakingHand(row_A, col_A);
-
-					int row_B, col_B;
-
-					std::cout << "" << std::endl << ">>";
-					std::cin >> row_B >> col_B;
-					
-					MatrixMakingHand(row_B, col_B);
-
-					VinogradAlgorithm(matrix_A, matirx_B, row_A, col_A, row_B, col_B);
-
-					break;
-				}
-
-				// from file
-				case 2:
-				{
-					std::string file_name;
-
-					std::cout << "" << std::endl << ">>";
-					std::cin >> file_name;
-
-					// тут будет работа с файлами
-
-					int row_A, col_A;
-
-					std::cout << "" << std::endl << ">>";
-					std::cin >> row_A >> col_A;
-
-					MatrixMakingFile(row_A, col_A);
-
-					int row_B, col_B;
-
-					std::cout << "" << std::endl << ">>";
-					std::cin >> row_B >> col_B;
-					
-					MatrixMakingFile(row_B, col_B);
-
-					VinogradAlgorithm(matrix_A, matirx_B, row_A, col_A, row_B, col_B);
-
-					break;
-				}
-
-				default:
-				{
-					std::cout << "" << std::endl;
-
-					break;
-				}
-			}		
-
-			break;
-		}
-
-		default:
-		{
-			std::cout << "" << std::endl;
-
-			break;
+			std::cin >> A[j][i];
 		}
 	}
 
-	return 0;
+	// Заполнение матрицы В
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j ++)
+		{
+			std::cin >> B[j][i];
+		}
+	}
+
+	// Вывод исходных матриц
+	std::cout << "Matrix A:" << std::endl;
+	std::cout << A[0][0] << " "; std::cout << A[0][1] << " " << std::endl;
+	std::cout << A[1][0] << " "; std::cout << A[1][1] << " " << std::endl;
+
+	std::cout << "Matrix B:" << std::endl;
+	std::cout << B[0][0] << " "; std::cout << B[0][1] << " " << std::endl;
+	std::cout << B[1][0] << " "; std::cout << B[1][1] << " " << std::endl;
+
+	// Strassen's idea
+	vector<vector <int>> C;
+
+	int P1, P2, P3, P4, P5, P6, P7;
+
+	P1 = A[0][0]*(B[0][1] - B[1][1]); 
+	P2 = (A[0][0] + A[0][1]) * B[1][1];
+	P3 = (A[1][0] + A[1][1]) * B[0][0];
+	P4 = A[1][1] * (B[1][0] - B[0][0]);
+	P5 = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
+	P6 = (A[0][1] - A[1][1]) * (B[0][1] + B[1][1]); 
+	P7 = (A[0][0] - A[1][0]) * (B[0][0] + B[0][1]);
+
+	C[1][1] = P5 + P4 - P2 + P6;
+	C[2][1] = P1 + P2;
+	C[1][2] = P3 + P4;
+	C[2][2] = P5 + P1 - P3 - P7;
+
+	// Вывод результата
+	std::cout << "Matrix C:" << std::endl;
+	std::cout << C[0][0] << " "; std::cout << C[0][1] << " " << std::endl;
+	std::cout << C[1][0] << " "; std::cout << C[1][1] << " " << std::endl;
 }
