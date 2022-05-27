7958e9eea1e64cb82216d840cfaa4b3c7bc72402
ForestGump <eremeevt2004@gmail.com>
2022-05-27 22:15:17 +0400

    Create main.cpp

---
+++ Инфопрога 2 сем/Хэширование/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+/*
+* Метод деления по месяцу рождения
+*/
+void task_1()
+{
+
+}
+
+/*
+* Основная - линейное хэширование
+* Вспомогательная - метод умножения по году
+*/
+void task_2()
+{
+
+} 
+
+// Функция исправляющая кализию
+// хз как она должна работать
+void ColishionFixer()
+{
+
+}
+
+void OpenHashMaker(string array[], int arraySize, int hashSize)
+{
+
+}
+
+int main()
+{
+    int chouser;
+
+    std::cout << "" << std::endl << ">>";
+    std::cin >> chouser;
+
+    switch (chouser)
+    {
+        // открытое хэширование
+        case 1:
+        {
+            break;
+        }
+
+        // закрытое хэширование
+        case 2:
+        {
+            break;
+        }
+        
+        default:
+        {
+            std::cout << "" << std::endl;
+
+            break;
+        }
+    }
+
+    return 0;
+}
